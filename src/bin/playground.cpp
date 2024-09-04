52c973a0da3fcebd43d31ec675c6050db7178eff
Stefan Halfpap <klauck2@gmail.com>
2024-09-04 16:19:31 +0200

    Add DuckDB dependencies to run the parser

---
+++ src/bin/playground.cpp
@@ -0,0 +1,12 @@
+#include "postgres_parser.hpp"
+
+#include <iostream>
+
+int main() {
+
+	duckdb::PostgresParser parser;
+
+	parser.Parse("SELECT * FROM t1;");
+	std::cout << parser.success << std::endl;
+
+}
