cb6e52afc74d16c95f6e97a5e24ba79f48391533
n1developer-ubt <u.b.t.usama.bin.tariq@gmail.com>
2024-09-16 13:53:13 +0200

    binder & catalog test added

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -5,12 +5,20 @@
 #include <duckdb/parser/statement/update_statement.hpp>
 #include <nodes/parsenodes.hpp>
 #include <list>
+#include <duckdb/common/serializer/serializer.hpp>
+#include <duckdb/common/tree_renderer/json_tree_renderer.hpp>
+#include <duckdb/execution/operator/helper/physical_result_collector.hpp>
+#include <duckdb/execution/operator/schema/physical_create_table.hpp>
 #include <duckdb/main/client_context.hpp>
 #include <duckdb/main/connection.hpp>
 #include <duckdb/main/database.hpp>
+#include <duckdb/optimizer/optimizer.hpp>
 #include <duckdb/planner/binder.hpp>
 #include <duckdb/planner/planner.hpp>
 #include <parser/parser.hpp>
+#include <vector>
+#include <duckdb/main/attached_database.hpp>
+#include <duckdb/parser/parsed_data/create_schema_info.hpp>
 
 using namespace duckdb;
 using namespace std;
@@ -284,7 +292,6 @@ static int ParseUsingPGParser(const string &query) {
 
 #pragma endregion
 
-
 #pragma region Binder
 static void DuckDbBinderTest() {
     DuckDB db(nullptr);
@@ -315,16 +322,176 @@ static void DuckDbBinderTest() {
 
     context->transaction.BeginTransaction();
     BoundStatement bounded_create_statement = binder->Bind(*create_sql_statement);
+
+}
+#pragma endregion
+
+#pragma region Planner
+
+int PrintPlan(const LogicalOperator &plan) {
+    JSONTreeRenderer renderer{};
+    const auto result = renderer.ToString(plan);
+
+    cout << result << endl;
+
+    return 0;
+}
+
+int PlannerTest(const string &query) {
+    //init database
+    DuckDB db(nullptr);
+
+    //init db connection
+    Connection con(db);
+
+    //execute the query so that we can create bound statement without exceptions
+    con.Query(query);
+
+    auto &database_instance = db.instance;
+    //create a database context, in our case it's a in memory database
+    const auto context = make_shared_ptr<ClientContext>(database_instance->shared_from_this());
+    //create planner instance
+    Planner planner(*context);
+    Parser parser;
+
+    //parse query
+    parser.ParseQuery(query);
+
+    //check if the statements are successfully parsed
+    if(parser.statements.empty()) {
+        cout << "Unable to parse statements";
+        return -1;
+    }
+
+    //create plan for each statement
+    for (auto &statement : parser.statements) {
+        cout << "Planning statement: " << statement->ToString() << endl;
+        auto stmt = move(statement);
+
+        //start a transaction
+        context->transaction.BeginTransaction();
+        //create plan using planner
+        planner.CreatePlan(move(stmt));
+
+        //get generated plan
+        auto &plan = planner.plan;
+
+        //verify plan
+        Planner::VerifyPlan(*context, plan);
+        cout << "Plan Verified" << endl;
+
+        cout << "Plan: ";
+        //print plan
+        PrintPlan(*plan);
+
+        //check if optimization is required
+        if (plan->RequireOptimizer()) {
+            cout << "Optimization Required" << endl;
+
+            //init optimizer
+            Optimizer optimizer(*planner.binder, *context);
+            //optimize the plan using optimizer
+            plan = optimizer.Optimize(std::move(plan));
+
+            //verify plan
+            Planner::VerifyPlan(*context, plan);
+            cout << "New Plan Verified" << endl;
+
+            cout << "New Plan: ";
+            //print new plan
+            PrintPlan(*plan);
+        }
+
+
+        PhysicalPlanGenerator physical_planner(*context);
+
+        const auto &physical_plan = physical_planner.CreatePlan(move(plan));
+
+        if(physical_plan->type == PhysicalOperatorType::CREATE_TABLE) {
+            cout << "Create Table" << endl;
+            const auto &physical_create_plan = physical_plan->Cast<PhysicalCreateTable>();
+            const auto &catalog = physical_create_plan.schema.catalog;
+
+            // auto schemas = catalog.GetAllSchemas(*context);
+            //
+            // for (const auto &schema: schemas) {
+            //
+            // }
+
+        }
+       // const auto &catalog = physical_plan-
+        // auto &result_collector = pysical_plan->Cast<PhysicalResultCollector>();
+        //
+        // auto res = result_collector.GetResult(*result_collector.sink_state);
+        //
+        // cout << res << endl;
+        //physical_plan->sche
+
+        context->transaction.Commit();
+
+        cout << "=======End of Query======" << endl;
+        break;
+    }
+
+    return 0;
+}
+#pragma endregion
+
+#pragma region Catalog
+int CatalogTest(const string &query) {
+    cout << "======Testing Catalog======" << endl;
+    DuckDB db(nullptr);
+    auto &database_instance = db.instance;
+    auto context = make_shared_ptr<ClientContext>(database_instance->shared_from_this());
+
+    //starting a transaction
+    context->transaction.BeginTransaction();
+
+    //create attached database instance from main db instance
+    AttachedDatabase attached_database(*db.instance);
+    //get the catalog from the attached database
+    auto &catalog = Catalog::GetCatalog(attached_database);
+
+    //get catalog transaction using active context
+    const auto &catalog_trans = catalog.GetCatalogTransaction(*context);
+
+    //create new schema information
+    CreateSchemaInfo info;
+    info.schema = "TEST SCHEMA";
+    info.internal = true;
+
+    //create schema in the catalog
+    catalog.CreateSchema(catalog_trans, info);
+
+    //commit transaction so that test schema being reflected in the catalog schemas
+    context->transaction.Commit();
+
+    context->transaction.BeginTransaction();
+    //get schema from catalog
+    const auto schemas = catalog.GetSchemas(*context);
+    cout << "Schemas: " << endl;
+    for (const auto &schema: schemas) {
+        cout << "===" << schema.get().name <<"===" << endl;
+        const auto sql = schema.get().ToSQL();
+
+        cout << "SQL: " << sql << endl;
+        cout << "===" << "END SCHEMA" << "==="<< endl << endl;
+    }
+
+
+    context->transaction.Commit();
+    return 0;
 }
 #pragma endregion
 
 int main() {
-    const std::string query = "SELECT abc, def, ghq FROM nebula_data, xyz;update nebula_data set a = 1 where b = 2;";
-
+    const std::string query = "CREATE TABLE X(abc INT);SELECT abc, def, ghq FROM nebula_data, xyz;update nebula_data set a = 1 where b = 2;";
+    const std::string test_query = "CREATE TABLE employees (id INT, name VARCHAR, department VARCHAR, salary DOUBLE, hire_date DATE, role VARCHAR); INSERT INTO employees VALUES (1, 'Alice', 'HR', 55000, '2020-01-15', 'Admin'), (2, 'Bob', 'Engineering', 70000, '2019-06-12', 'Other'), (3, 'Carol', 'HR', 60000, '2021-03-18', 'Admin'), (4, 'Dave', 'Sales', 48000, '2018-11-24', 'Other');";
     //ParseUsingDuckDB(query);
-    ParseUsingPGParser(query);
-
+    //ParseUsingPGParser(query);
     //DuckDbBinderTest();
+    PlannerTest(test_query);
+    //CatalogTest(test_query);
 
     return 0;
 }
